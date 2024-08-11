223d26fb3254144bc9ba94e0d2c30c83d0159e35
Ryan <ryanshaunsmith@gmail.com>
2024-08-11 20:55:49 +0100

    started post function

--- resources/learnings.js
+++ resources/learnings.js
@@ -2,8 +2,20 @@
 import { pool } from "../db/index.js";
 
 // Helper function to get all learnings
-export async function getModules() {
-    const query = "SELECT * FROM learnings";
+export async function getLearnings() {
+  const query = "SELECT * FROM learnings";
+  try {
     const result = await pool.query(query);
     return result.rows;
+  } catch (e) {
+    console.error("Error executing query", {
+      message: error.message,
+      stack: error.stack,
+      query,
+    });
+    throw error;
+  }
 }
+
+// Helper function to post a new learning
+export async function create(data) {}
